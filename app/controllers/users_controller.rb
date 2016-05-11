3cc0be41bb46ff386c4f4018b02ee09d55be1c36
Rodolfo Peixoto - DELLI7INSPIRION <contato@rodolfopeixot.com.br>
2016-05-11 09:32:01 -0300

    Added search in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def search_users
+  	users   = [];
+  	@users  = User.ac_search(params[:username])
+  	@users.each do |u|
+      user = User.find(u.id)
+      users.push(user)
+  	end
+  	render json: users
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
+  get "/users/search" => "users#search_users"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
